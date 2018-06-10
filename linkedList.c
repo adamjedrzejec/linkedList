9b7d5f8162b435d0cddd8ce4deec064e59f7cf49
adamjedrzejec <adamjedrzejec@o2.pl>
2018-06-11 00:32:01 +0200

    To do: cpoy, reverse, sort

--- linkedList.c
+++ linkedList.c
@@ -10,26 +10,35 @@ Node *init();
 int length(Node *);
 void display(Node *);
 void push(Node **, char *);
-
+char *pop(Node *);
+void destroy(Node *);
+void append(Node *, char *);
+//copy
+//reverse
+//sort
 
 
 int main (int argc, const char **argv) {
   Node *testNode = init();
-  //testNode -> string = "test";
+  testNode -> string = "test";
 
   push(&testNode, "test222");
-
   push(&testNode, "test2352");
   display(testNode);
+  destroy(testNode);
 
-  free(testNode -> nextNode);
-  free(testNode);
+  printf("\n\n");
 
+  testNode = init();
+  push(&testNode, "test1");
+  push(&testNode, "test2");
+  push(&testNode, "test3");
+  append(testNode, "test4");
+  printf("taken: %s\n", pop(testNode));
+  display(testNode);
+  destroy(testNode);
   return 0;
 }
-
-
-
 
 
 
@@ -79,4 +88,53 @@ void push(Node **linkedList, char *string) {
     pushNode -> nextNode = *linkedList;
     *linkedList = pushNode;
   }
+}
+
+char *pop(Node *linkedList){
+  Node *currentNode = linkedList, *temporaryNode;
+  char *stringToReturn;
+  int i;
+
+  if(currentNode -> nextNode == NULL){ // for list of 1 element
+    stringToReturn = currentNode -> string;
+    free(currentNode);
+    return stringToReturn;
+  }
+
+  if(currentNode -> nextNode != NULL)
+    while((currentNode -> nextNode) -> nextNode != NULL){ currentNode = currentNode -> nextNode; }
+
+  //if(currentNode -> nextNode != NULL){ // if linkedList is bigger than 1 element long, then currentNode is set on penultimate node
+    temporaryNode = currentNode;
+    currentNode = currentNode -> nextNode; // setting currentNode on last position
+    temporaryNode -> nextNode = NULL;
+  //}
+
+  //if(currentNode -> nextNode == NULL){
+    stringToReturn = currentNode -> string;
+    free(currentNode);
+    return stringToReturn;
+  //}
+}
+
+void destroy(Node *linkedList){
+  int counter;
+  Node *lastNode = linkedList, *nodeToFree;
+  while(lastNode -> nextNode != NULL){
+    nodeToFree = lastNode;
+    lastNode = lastNode -> nextNode;
+    free(nodeToFree);
+  }
+  free(lastNode);
+}
+
+void append(Node *linkedList, char *string){
+  Node *currentNode = linkedList, *newNode = malloc(sizeof(Node));
+
+  while(currentNode -> nextNode != NULL) { currentNode = currentNode -> nextNode; }
+
+  newNode -> nextNode = NULL;
+  newNode -> string = string;
+
+  currentNode -> nextNode = newNode;
 }
